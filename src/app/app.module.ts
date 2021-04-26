a97e5e5d94b219d3e64a1fd40d58adc1acadbfae
HARSHENI <harsheni.chandran@augustahitech.com>
2021-04-26 11:13:16 +0530

    packages updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { ReceptionistComponent } from './receptionist/receptionist.component';
+import { ManagementComponent } from './management/management.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DoctorComponent,
+    ReceptionistComponent,
+    ManagementComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
