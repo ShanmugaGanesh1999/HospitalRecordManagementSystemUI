ca4a13bc2cd703ef7fd55232040b3d7db27020d8
SHANMUGA GANESH <shanmuga.ganesh@mailfence.com>
2021-05-10 10:56:28 +0530

    order matters

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,11 @@ import { ManagementComponent } from './management/management.component';
 import { ReceptionistComponent } from './receptionist/receptionist.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'doctor-page', component: DoctorComponent },
   { path: 'receptionist-page', component: ReceptionistComponent },
   { path: 'management-page', component: ManagementComponent },
   { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', component: LoginComponent },
 ];
 
